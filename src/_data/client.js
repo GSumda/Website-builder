88210f3c6db4ce3aec978be6595d0ab6e02c3b32
GSumda <georgesumda@gmail.com>
2025-09-04 23:05:34 +0100

    content changes

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,20 +1,17 @@
 module.exports = {
-    name: "Code Stitch Web Designs",
-    email: "help@codestitch.app",
-    phoneForTel: "555-779-4407",
+    name: "RD Painting & Property Maintenance",
+    email: "Enquiries@Turbotugjobs.co.uk",
+    phoneForTel: "",
     phoneFormatted: "(555) 779-4407",
     address: {
         lineOne: "First Address Line",
         lineTwo: "Second Address Line",
-        city: "Denver",
-        state: "CO",
-        zip: "80206",
-        country: "US",
+    
         mapLink: "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6",
     },
     socials: {
         facebook: "https://www.facebook.com/",
-        instagram: "https://www.instagram.com/",
+        instagram: "https://www.instagram.com/rdpainting_propertymaintenance/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
     domain: "https://www.example.com",
